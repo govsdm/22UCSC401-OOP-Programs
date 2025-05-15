dfe9216975d989b1837ce0c994f5054454291bc1
govsdm <govsdm@gmail.com>
2025-05-15 05:34:40 +0530

    Collections

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -1,23 +1,18 @@
 package hashMap;
-
 import java.util.*;
-
 class HashMapDemo {
 	public static void main(String args[]) {
-
 		// Create a hash map.
 		HashMap<String, Double> hm = new HashMap<String, Double>();
-
 		// Put elements to the map
 		hm.put("John Doe", 3434.34);
 		hm.put("Tom Smith", 123.22);
 		hm.put("Jane Baker", 1378.00);
 		hm.put("Tod Hall", 99.22);
 		hm.put("Ralph Smith", -19.08);
-
+		
 		// Get a set of the entries.
 		Set<Map.Entry<String, Double>> set = hm.entrySet();
-
 		// Display the set.
 		for (Map.Entry<String, Double> me : set) {
 			System.out.print(me.getKey() + ": ");
@@ -27,7 +22,6 @@ class HashMapDemo {
 
 		System.out.println("Dispaly elements of Map using iterator:");
 		Iterator<Entry<String, Double>> itr = set.iterator();
-		
 		while(itr.hasNext()) {
 			Map.Entry<String, Double> entry = itr.next();
 			System.out.print(entry.getKey() + ": " + entry.getValue());
